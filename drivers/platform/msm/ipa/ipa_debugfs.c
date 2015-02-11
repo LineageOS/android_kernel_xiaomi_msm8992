af22e9c7ee10503673769c5b94091041362d24b4
Skylar Chang <chiaweic@codeaurora.org>
2015-02-11 10:19:11 -0800

    msm: ipa: Handle NAT immediate commands during xlat
    There is no guarantee that NAT entry update will be atomic.
    So it is possible for an XLAT packet thus to be updated
    according to partial NAT configuration. Send ‘NOP’ Register
    Write immediate command with wait for pipeline clear just
    before the NAT immediate commands.
    
    Change-Id: Idadc2bddb73e8bd61a3b7b04bbd9e59b14d7ff1b
    Acked-by: Sunil Kumar Paidimarri <hisunil@qti.qualcomm.com>
    Signed-off-by: Skylar Chang <chiaweic@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_debugfs.c
+++ drivers/platform/msm/ipa/ipa_debugfs.c
@@ -1271,6 +1271,7 @@ static ssize_t ipa_read_nat4(struct file *file,
 	u32 tbl_size, *tmp;
 	u32 value, i, j, rule_id;
 	u16 enable, tbl_entry, flag;
+	u32 no_entrys = 0;
 
 	value = ipa_ctx->nat_mem.public_ip_addr;
 	pr_err(
@@ -1284,7 +1285,7 @@ static ssize_t ipa_read_nat4(struct file *file,
 				ipa_ctx->nat_mem.size_base_tables);
 
 	pr_err("Expansion Table Size:%d\n",
-				ipa_ctx->nat_mem.size_expansion_tables);
+				ipa_ctx->nat_mem.size_expansion_tables-1);
 
 	if (!ipa_ctx->nat_mem.is_sys_mem)
 		pr_err("Not supported for local(shared) memory\n");
@@ -1298,7 +1299,7 @@ static ssize_t ipa_read_nat4(struct file *file,
 
 			pr_err("\nBase Table:\n");
 		} else {
-			tbl_size = ipa_ctx->nat_mem.size_expansion_tables;
+			tbl_size = ipa_ctx->nat_mem.size_expansion_tables-1;
 			base_tbl =
 			 (u32 *)ipa_ctx->nat_mem.ipv4_expansion_rules_addr;
 
@@ -1312,6 +1313,7 @@ static ssize_t ipa_read_nat4(struct file *file,
 				enable = ((value & 0xFFFF0000) >> 16);
 
 				if (enable & NAT_ENTRY_ENABLE) {
+					no_entrys++;
 					pr_err("Rule:%d ", rule_id);
 
 					value = *tmp;
@@ -1396,7 +1398,7 @@ static ssize_t ipa_read_nat4(struct file *file,
 
 			pr_err("\nIndex Table:\n");
 		} else {
-			tbl_size = ipa_ctx->nat_mem.size_expansion_tables;
+			tbl_size = ipa_ctx->nat_mem.size_expansion_tables-1;
 			indx_tbl =
 			 (u32 *)ipa_ctx->nat_mem.index_table_expansion_addr;
 
@@ -1423,6 +1425,7 @@ static ssize_t ipa_read_nat4(struct file *file,
 			}
 		}
 	}
+	pr_err("Current No. Nat Entries: %d\n", no_entrys);
 
 	return 0;
 }
