d59af85fb0c74aa3e04a33dc572ef39a402b066f
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-04-02 16:33:29 +0700

    pwm: pwm-qpnp: Apply Xiaomi changes
    Change-Id: Ib460a1f75db93042b051e05a76f44d556fdf411d
    Signed-off-by: Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>

--- drivers/pwm/pwm-qpnp.c
+++ drivers/pwm/pwm-qpnp.c
@@ -532,8 +532,15 @@ static void qpnp_lpg_calc_period(enum time_level tm_lvl,
 			} else if (best_m >= 1 && (
 				chip->sub_type != QPNP_PWM_MODE_ONLY_SUB_TYPE &&
 				chip->sub_type != QPNP_LPG_S_CHAN_SUB_TYPE)) {
+#ifdef CONFIG_MACH_XIAOMI_MSM8992
+				if (supported_sizes == QPNP_PWM_SIZE_6_7_9_BIT) {
+					/* Let n=7 only when 7bit is supported */
+#endif
 				n += 1;
 				best_m -= 1;
+#ifdef CONFIG_MACH_XIAOMI_MSM8992
+				}
+#endif
 			}
 		}
 	}
